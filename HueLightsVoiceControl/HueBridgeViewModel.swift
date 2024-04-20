c4a1e924701fe45fab1c892946bb035eb5b8a7ab
Samuil Dimov <122555034+SamuilDimov@users.noreply.github.com>
2024-04-20 12:26:37 +0200

    Color chaning is working
    Time to work on the voice control

--- HueLightsVoiceControl/HueBridgeViewModel.swift
+++ HueLightsVoiceControl/HueBridgeViewModel.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Speech
 
 class HueBridgeViewModel: ObservableObject {
     @Published var bridgeIP: String?
@@ -8,145 +9,132 @@ class HueBridgeViewModel: ObservableObject {
 
     func discoverHueBridge() {
         let url = URL(string: "https://discovery.meethue.com/")!
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data else {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                do {
+                    let bridges = try JSONDecoder().decode([Bridge].self, from: data)
+                    DispatchQueue.main.async {
+                        self.bridgeIP = bridges.first?.internalipaddress
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.authenticationError = "JSON decoding error: \(error)"
+                    }
+                }
+            } else {
                 DispatchQueue.main.async {
                     self.authenticationError = error?.localizedDescription ?? "Unknown error"
                 }
-                return
             }
-            do {
-                let bridges = try JSONDecoder().decode([Bridge].self, from: data)
-                DispatchQueue.main.async {
-                    self.bridgeIP = bridges.first?.internalipaddress
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.authenticationError = "JSON decoding error: \(error)"
-                }
-            }
-        }
-        task.resume()
+        }.resume()
     }
 
     func createUser(ipAddress: String) {
         let url = URL(string: "http://\(ipAddress)/api")!
         let body = ["devicetype": "my_hue_app#ios"]
-        guard let requestBody = try? JSONEncoder().encode(body) else { return }
-        
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
-        request.httpBody = requestBody
+        request.httpBody = try? JSONEncoder().encode(body)
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    let result = try JSONDecoder().decode([CreateUserResponse].self, from: data)
+                    DispatchQueue.main.async {
+                        self.username = result.first?.success?.username
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.authenticationError = "JSON decoding error: \(error)"
+                    }
+                }
+            } else {
                 DispatchQueue.main.async {
                     self.authenticationError = error?.localizedDescription ?? "Unknown error"
                 }
-                return
             }
-            do {
-                let result = try JSONDecoder().decode([CreateUserResponse].self, from: data)
-                DispatchQueue.main.async {
-                    self.username = result.first?.success?.username
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.authenticationError = "JSON decoding error: \(error)"
-                }
-            }
-        }
-        task.resume()
+        }.resume()
     }
 
     func fetchLights() {
         guard let ipAddress = bridgeIP, let username = username else {
-            self.authenticationError = "No bridge IP or username"
+            DispatchQueue.main.async {
+                self.authenticationError = "No bridge IP or username"
+            }
             return
         }
         let url = URL(string: "http://\(ipAddress)/api/\(username)/lights")!
+        let request = URLRequest(url: url)
         
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard let data = data else {
-                DispatchQueue.main.async {
-                    self.authenticationError = error?.localizedDescription ?? "Unknown error"
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                do {
+                    let lightsDictionary = try JSONDecoder().decode([String: LightData].self, from: data)
+                    DispatchQueue.main.async {
+                        self.lights = lightsDictionary.map { id, lightData in
+                            Light(id: id, state: lightData.state)
+                        }.sorted(by: { $0.id < $1.id })
+                    }
+                } catch {
+                    DispatchQueue.main.async {
+                        self.authenticationError = "JSON decoding error: \(error.localizedDescription)"
+                    }
                 }
-                return
-            }
-            do {
-                let lightsDictionary = try JSONDecoder().decode([String: Light].self, from: data)
+            } else {
                 DispatchQueue.main.async {
-                    self.lights = lightsDictionary.map { id, light in
-                        var lightCopy = light
-                        lightCopy.id = id
-                        return lightCopy
-                    }.sorted(by: { $0.id < $1.id })
-                }
-            } catch {
-                DispatchQueue.main.async {
-                    self.authenticationError = "JSON decoding error: \(error)"
+                    self.authenticationError = error?.localizedDescription ?? "Unknown error fetching lights."
                 }
             }
-        }
-        task.resume()
+        }.resume()
     }
 
     func setLightState(lightId: String, isOn: Bool, hue: Int? = nil, saturation: Int? = nil, brightness: Int? = nil) {
         guard let ipAddress = bridgeIP, let username = username else {
-            self.authenticationError = "No bridge IP or username"
+            DispatchQueue.main.async {
+                self.authenticationError = "No bridge IP or username"
+            }
             return
         }
         let url = URL(string: "http://\(ipAddress)/api/\(username)/lights/\(lightId)/state")!
         var state: [String: Any] = ["on": isOn]
-        if let hue = hue {
-            state["hue"] = hue
-        }
-        if let saturation = saturation {
-            state["sat"] = saturation
-        }
-        if let brightness = brightness {
-            state["bri"] = brightness
-        }
-        
-        guard let requestBody = try? JSONSerialization.data(withJSONObject: state) else { return }
-        
+        if let hue = hue { state["hue"] = hue }
+        if let saturation = saturation { state["sat"] = saturation }
+        if let brightness = brightness { state["bri"] = brightness }
         var request = URLRequest(url: url)
         request.httpMethod = "PUT"
-        request.httpBody = requestBody
+        request.httpBody = try? JSONSerialization.data(withJSONObject: state)
         
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        URLSession.shared.dataTask(with: request) { data, response, error in
             if let error = error {
                 DispatchQueue.main.async {
                     self.authenticationError = "Error setting light state: \(error.localizedDescription)"
                 }
             }
-        }
-        task.resume()
+        }.resume()
     }
 }
 
-struct Bridge: Codable {
-    let id: String
+struct Bridge: Decodable {
     let internalipaddress: String
 }
 
-struct CreateUserResponse: Codable {
-    struct Success: Codable {
+struct CreateUserResponse: Decodable {
+    struct Success: Decodable {
         let username: String
     }
     let success: Success?
 }
 
-struct Light: Identifiable, Codable {
+struct Light: Identifiable {
     var id: String
-    var name: String
     var state: LightState
 }
 
-struct LightState: Codable {
+struct LightData: Decodable {
+    var state: LightState
+}
+
+struct LightState: Decodable {
     var on: Bool
     var bri: Int
     var hue: Int?
